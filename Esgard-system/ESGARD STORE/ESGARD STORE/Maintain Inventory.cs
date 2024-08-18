98d7c195332d37b8934a8dfd09a69ddba04b3cb2
ASSteenbok <143697783+ASSteenbok@users.noreply.github.com>
2024-08-18 17:35:02 +0200

    Add files via upload

---
+++ Esgard-system/ESGARD STORE/ESGARD STORE/Maintain Inventory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESGARD_STORE
+{
+    public partial class Maintain_Inventory : Form
+    {
+        public Maintain_Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void Maintain_Inventory_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Dashboard ds = new Dashboard();
+            //ds.ShowDialog();
+
+
+            Dashboard ds = Application.OpenForms["Dashboard"] as Dashboard;
+
+
+            if (ds == null)
+            {
+                ds = new Dashboard();
+                ds.Show();
+            }
+            else
+            {
+                ds.BringToFront();
+            }
+
+
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Dashboard ds = new Dashboard();
+            //ds.ShowDialog();
+
+            Dashboard ds = Application.OpenForms["Dashboard"] as Dashboard;
+
+
+            if (ds == null)
+            {
+                ds = new Dashboard();
+                ds.Show();
+            }
+            else
+            {
+                ds.BringToFront();
+            }
+
+
+            this.Close();
+        }
+    }
+}
---
+++ Esgard-system/ESGARD STORE/ESGARD STORE/Returns.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ESGARD_STORE
+{
+    partial class Returns
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSClientN = new System.Windows.Forms.TextBox();
+            this.btnMReturn = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(697, 58);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(373, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Client Number:";
+            // 
+            // txtSClientN
+            // 
+            this.txtSClientN.Location = new System.Drawing.Point(511, 61);
+            this.txtSClientN.Name = "txtSClientN";
+            this.txtSClientN.Size = new System.Drawing.Size(147, 20);
+            this.txtSClientN.TabIndex = 9;
+            // 
+            // btnMReturn
+            // 
+            this.btnMReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMReturn.Location = new System.Drawing.Point(634, 458);
+            this.btnMReturn.Name = "btnMReturn";
+            this.btnMReturn.Size = new System.Drawing.Size(114, 23);
+            this.btnMReturn.TabIndex = 24;
+            this.btnMReturn.Text = "Make Return";
+            this.btnMReturn.UseVisualStyleBackColor = true;
+            this.btnMReturn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label14.Location = new System.Drawing.Point(364, 503);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(265, 16);
+            this.label14.TabIndex = 26;
+            this.label14.Text = "Tel. 0726168472 email. whatisit@gmail.com";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label15.Location = new System.Drawing.Point(108, 503);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(233, 16);
+            this.label15.TabIndex = 27;
+            this.label15.Text = "@2024 Esgard, inc, All rights reserved";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ESGARD_STORE.Properties.Resources.ESGARD_LOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(760, 329);
+            this.listBox1.TabIndex = 28;
+            // 
+            // Returns
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 528);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnMReturn);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSClientN);
+            this.Name = "Returns";
+            this.Text = "Returns";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSClientN;
+        private System.Windows.Forms.Button btnMReturn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
